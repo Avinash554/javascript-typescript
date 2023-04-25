f6b81dfdad0c2dd7d0ef2c688666a833d6e8bad6
Avinash Maricharla <avin9235@gmail.com>
2023-04-25 16:37:47 -0400

    intial commit

---
+++ leetcode_practice/array/lc_best_time_to_buy_and_sell_stock.ts
@@ -0,0 +1,103 @@
+
+
+
+
+
+/** Leetcode: 121.Best Time to Buy and Sell Stock */
+/** Solution 1: Using 'for loops' */
+/**
+let prices = [7, 1, 5, 3, 6, 4];
+
+var maxProfit = function (prices) {
+    let profit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        const buyPrice = prices[i];
+        for (let j = i + 1; j < prices.length; j++) {
+            const currentProfit = prices[j] - buyPrice; // consider prices[j] --> sell price
+            if (currentProfit > profit) {
+                profit = currentProfit;
+            }
+        }
+    }
+    return profit;
+};
+*/
+
+/**
+Notes:
+    Time complexity: quadratic O(n^2) because of '2 for loops'.
+    1-7 = -6
+    5-7 = -2
+    3-7 = -4
+    6-7 = -1
+    4-7 = -3
+ */
+/***************************** Solution 1: End *****************************/
+/** Solution 2: Using 'for loop' */
+/** 
+let prices = [7, 1, 5, 3, 6, 4];
+
+var maxProfit = function (prices) {
+    let profit = 0;
+    let buyPrice = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        if (buyPrice > prices[i]) { // consider prices[i] --> sell price
+            buyPrice = prices[i];
+        }
+        const currentProfit = prices[i] - buyPrice;
+        if (currentProfit > profit) {
+            profit = currentProfit;
+        }
+    }
+    return profit;
+};
+*/
+
+/**
+Notes:
+    Time complexity: linear-O(n) because of '1 for loop'.
+    Loop 1: 
+        stockToBuy 7 > 1 then stockToBuy = 1
+        currentProfit = 1 - 1; // 0
+        profit 0;
+    Loop 2:
+        stockToBuy 1 > 5 then stockToBuy = 1 // previous loop value
+        currentProfit = 5 - 1; // 4
+        profit 4;
+    Loop 3:
+        stockToBuy 1 > 3 then stockToBuy = 1 // previous loop value
+        currentProfit = 3 - 1; // 2
+        profit 4;
+    Loop 4:
+        stockToBuy 1 > 6 then stockToBuy = 1 // previous loop value
+        currentProfit = 6 - 1; // 5
+        profit 5;
+    Loop 5:
+        stockToBuy 1 > 4 then stockToBuy = 1 // previous loop value
+        currentProfit = 4 - 1; // 3
+        profit 5;
+
+    https://relatablecode.com/javascript-leetcode-best-time-to-buy-and-sell-stock
+ */
+/***************************** Solution 2: End *****************************/
+/** Solution 3: using 'max & min' methods */
+let prices = [7, 1, 5, 3, 6, 4];
+
+var maxProfit = function (prices) {
+    let profit = 0;
+    let priceArr;
+    for (let i = 0; i < prices.length; i++) {
+        const buyPrice = prices[i];
+        priceArr = Math.max(...prices);
+
+    }
+    return profit;
+};
+
+/**
+Notes:
+    Other solutions you can work on:
+    https://www.educative.io/answers/what-are-mathmin-and-mathmax-in-javascript
+    Using max and min: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39267/javascript-solution-if-anyone-is-interested/
+ */
+/***************************** Solution 3: End *****************************/
---
+++ leetcode_practice/linked_list/lc_merge_two_sorted_lists.ts
@@ -0,0 +1,31 @@
+// Concepts to learn and understand: Linked List
+
+
+
+/** LeetCode: 21.Merge Two Sorted Lists */
+/** Solution 1: Using 'for loop & sort()' method */
+let list1 = [1, 2, 4];
+let list2 = [1, 3, 4];
+
+var mergeTwoLists = function (list1, list2) {
+    if (!list1.length && !list2.length) {
+        return [];
+    }
+    for (let i = 0; i < list2.length; i++) {
+        list1.push(list2[i]);
+    }
+    return list1.sort();
+};
+
+/**
+Notes:
+    Time complexity: linear O(n)
+
+
+    https://leetcode.com/problems/merge-two-sorted-lists/solutions/341711/clean-javascript/
+    https://leetcode.com/problems/merge-two-sorted-lists/solutions/9963/simple-javascript-solution/
+    https://leetcode.com/problems/merge-two-sorted-lists/solutions/238175/10-line-javascript-recursion/
+    https://leetcode.com/problems/merge-two-sorted-lists/solutions/1131818/javascript-faster-than-100/
+
+ */
+/***************************** Solution 1: End *****************************/
